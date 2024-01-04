c597108023ce6b46346fa53cd7c0fc90dbfcb8b4
Ruben Franco <tinchopmanya@gmail.com>
2024-01-04 01:00:35 -0300

    mas cruds

--- build.sh
+++ build.sh
@@ -6,3 +6,7 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+if [[ $CREATE_SUPERUSER ]];
+then
+  python world_champ_2022/manage.py createsuperuser --no-input
+fi
