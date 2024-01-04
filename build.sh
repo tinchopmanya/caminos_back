5a03308f8f963481908674fa34968ca627357fd7
Ruben Franco <tinchopmanya@gmail.com>
2024-01-04 02:23:04 -0300

    superusuarios

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-DJANGO_SUPERUSER_USERNAME=testuser \
-DJANGO_SUPERUSER_PASSWORD=testpass \
-DJANGO_SUPERUSER_EMAIL="admin@admin.com" \
+DJANGO_SUPERUSER_USERNAME=testuser 
+DJANGO_SUPERUSER_PASSWORD=testpass 
+DJANGO_SUPERUSER_EMAIL="admin@admin.com" 
 python3 manage.py createsuperuser --no-input
