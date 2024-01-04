1b996eca90e075ca3f2766e5370ac86eec87fc03
Ruben Franco <tinchopmanya@gmail.com>
2024-01-04 01:06:47 -0300

    superusuarios

--- build.sh
+++ build.sh
@@ -3,10 +3,6 @@
 set -o errexit
 
 pip install -r requirements.txt
-
 python manage.py collectstatic --no-input
 python manage.py migrate
-if [[ $CREATE_SUPERUSER ]];
-then
-  python world_champ_2022/manage.py createsuperuser --no-input
-fi
+python manage.py createsuperuser --no-input
